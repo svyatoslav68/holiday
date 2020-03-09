8681a364125873e0c8ec2e054810aca23c0e090b
svyatoslav68 <svjat68@mail.ru>
2020-03-09 15:19:02 +0300

    Созданы конструкторы для THoliday

--- app/src/tholiday.cpp
+++ app/src/tholiday.cpp
@@ -3,9 +3,35 @@
 				    файл  tholiday.cpp
 ***********************************************************/
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
 #include "tholiday.hpp"
+#include "str_from_file.hpp"
+
+const std::string THoliday::templateSQLHoliday=StrFromFile("SQL.txt", ":").getString("getHoliday");
 
 THoliday::THoliday(){
 
 }
 
+THoliday::THoliday(int cod_holiday){
+	data_from_BD = nullptr;
+	boost::format fmter(templateSQLHoliday);
+	std::stringstream ss;
+	ss << fmter%cod_holiday;
+	std::string SQL = ss.str();
+	int mysql_status = 0;
+	mysql_status = mysql_query(appParametrs.getDescriptorBD(), SQL.c_str());
+	if (mysql_status){
+		std::cout << "Ошибка при выполнении запроса: " << SQL << std::endl;
+	}
+	else {
+		data_from_BD = mysql_store_result(appParametrs.getDescriptorBD());
+		MYSQL_ROW row;
+		row = mysql_fetch_row(data_from_BD);
+		name_holiday = row[1];
+		cod_person = boost::lexical_cast<int>(row[2]);		
+		date_begin = from_simple_string(std::string(row[3]));
+		holiday_duration = date_duration(boost::lexical_cast<int>(row[4])+boost::lexical_cast<int>(row[5]));
+	}
+}
--- app/src/tholiday.hpp
+++ app/src/tholiday.hpp
@@ -4,15 +4,21 @@
 ***********************************************************/
 #ifndef THOLIDAY_HPP
 #define THOLIDAY_HPP
+
+#include "cl_parametrs.hpp"
 using bt=boost::gregorian::date;
 using namespace boost::gregorian;
+
+extern clParametrs appParametrs;
 
 class THoliday {
 public:
 	THoliday();
-	THoliday(int _cod_person):cod_person(_cod_person){};
+	THoliday(int cod_holiday);
 	THoliday(int _cod_person, std::string s_date, int duration, std::string _name_holiday):cod_person(_cod_person), date_begin(from_simple_string(std::string(s_date))), holiday_duration(date_duration(duration)), name_holiday(_name_holiday){};
 private:
+	MYSQL_RES *data_from_BD;
+	static const std::string templateSQLHoliday;
 	int cod_person;
 	bt date_begin;	
 	date_duration holiday_duration;
